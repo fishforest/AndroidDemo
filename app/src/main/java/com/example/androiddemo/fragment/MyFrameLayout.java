b86bcf2af6a8b66a6c9a54abca6ef3688c7d8928
fishforest <409652578@qqo.com>
2021-11-04 23:08:16 +0800

    fragment

---
+++ app/src/main/java/com/example/androiddemo/fragment/MyFrameLayout.java
@@ -0,0 +1,18 @@
+package com.example.androiddemo.fragment;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class MyFrameLayout extends FrameLayout {
+    public MyFrameLayout(@NonNull Context context) {
+        super(context);
+    }
+
+    public MyFrameLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
